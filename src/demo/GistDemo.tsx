d7a37ea1b98f522970eae256a1aab23d04e83863
huaweiwei <harvey0379@163.com>
2022-02-21 21:45:37 +0800

    feat: react-gist

---
+++ src/demo/GistDemo.tsx
@@ -0,0 +1,10 @@
+import Gist from 'react-gist';
+
+export default function GistDemo() {
+  return (
+    <fieldset>
+      <legend>Gist</legend>
+      <Gist id='61c605694eeebeea0bdf30877118493c' file='variables.scss' />
+    </fieldset>
+  );
+}
