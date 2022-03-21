f94fb2d2796b71fa0c754078dfe610521c1274dc
huaweiwei <harvey0379@163.com>
2022-03-21 12:35:14 +0800

    css;

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,12 @@ import { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import GistDemo from './demo/GistDemo';
+import CSSDashboard from './basic/CSSDashboard';
 
 function App() {
   return (
     <>
+      <CSSDashboard></CSSDashboard>
       <GistDemo></GistDemo>
     </>
   );
