81d412c2f47b4462dcdf33d90b792ea73db457d8
Harvey <harvey0379@163.com>
2022-10-23 21:26:52 +0800

    chore: add maojian

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import getGitInfo, { GitInfo, Params } from '@harvey0379/maojian'
 
+const param: Params = {}
+const gitInfo: GitInfo = getGitInfo(param)
+console.log(gitInfo)
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
 })
